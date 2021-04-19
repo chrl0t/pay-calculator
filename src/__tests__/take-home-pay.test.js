8cd862d319642f2f74aa20d590ec663573f09a1d
chrl0t <charlotte_i@live.co.uk>
2021-04-19 11:27:17 +0100

    Write first test

---
+++ src/__tests__/take-home-pay.test.js
@@ -0,0 +1,14 @@
+const { takeHomePay } = require("../utils/take-home-pay");
+
+describe("takeHomePay()", () => {
+  it("should return a warning if an invalid input is passed in", () => {
+    const input = "hi";
+    const expectedOutput = "Invalid input";
+    const acutalOutput = takeHomePay(input);
+    expect(acutalOutput).toEqual(expectedOutput);
+    const input1 = -1;
+    const expectedOutput1 = "Invalid input";
+    const acutalOutput1 = takeHomePay(input1);
+    expect(acutalOutput1).toEqual(expectedOutput1);
+  });
+});
---
+++ src/utils/take-home-pay.js
@@ -0,0 +1,3 @@
+export const takeHomePay = () => {
+  return "hi";
+};

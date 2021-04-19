db700bcc636205b9be5d30c9dbba5e29ab1c7124
chrl0t <charlotte_i@live.co.uk>
2021-04-19 11:36:55 +0100

    Pass third test

--- src/utils/take-home-pay.js
+++ src/utils/take-home-pay.js
@@ -2,7 +2,7 @@ const takeHomePay = (salary) => {
   if (typeof salary !== "number" || salary < 0) {
     return "Invalid input";
   } else {
-    return {};
+    return { tax: 0, ni: 0, pay: 0 };
   }
 };
 

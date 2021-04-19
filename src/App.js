58e5711a25dc092a01b97ba45b77ae23a4affd39
chrl0t <charlotte_i@live.co.uk>
2021-04-19 11:08:23 +0100

    Create and display Header component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-  return <h1>Pay Calculator</h1>;
+  return (
+    <div>
+      <Header />
+      <h1>Pay Calculator</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <h1>Gallagher Pay Calculator</h1>;
+};
+
+export default Header;

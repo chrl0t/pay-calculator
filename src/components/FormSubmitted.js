ebc9001484b3d5e2194db5547744b506e78c7f38
chrl0t <charlotte_i@live.co.uk>
2021-04-21 09:53:57 +0100

    Displaying correct tax, ni and pay on Form Submitted

--- src/components/FormSubmitted.js
+++ src/components/FormSubmitted.js
@@ -1,10 +1,18 @@
 import React from "react";
+import takeHomePay from "../utils/take-home-pay";
 
 const FormSubmitted = ({ grossSalary }) => {
+  const newGrossSalary = parseInt(grossSalary);
+  const payslip = takeHomePay(newGrossSalary);
+  const tax = payslip.tax;
+  const ni = payslip.ni;
+  const pay = payslip.pay;
   return (
     <div>
       <h1>Form submitted</h1>
-      <h1>{grossSalary}</h1>
+      <h1>tax: {tax}</h1>
+      <h1>ni: {ni}</h1>
+      <h1>pay: {pay}</h1>
     </div>
   );
 };
